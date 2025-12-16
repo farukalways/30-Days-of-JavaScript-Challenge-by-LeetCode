5487c87109e0ef26e405d1346d9f48f0cb185839
farukalways <alishopon367@gmail.com>
2025-12-16 08:35:03 +0600

    day 01- 4  problem solve no use ai

---
+++ problem-01.js
@@ -0,0 +1,21 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+// Example 1:
+
+// Input: args = []
+// Output: "Hello World"
+// Explanation:
+// const f = createHelloWorld();
+// f(); // "Hello World"
+
+// The function returned by createHelloWorld should always return "Hello World".
+
+const createHelloWorld = (args) => {
+  const outPut = () => {
+    return "Hello World";
+  };
+
+  return outPut;
+};
+
+createHelloWorld();
